a64abfb395dcbfe322b2a5787db55debb2c7f339
fewfw3r32 <69639077+fewfw3r32@users.noreply.github.com>
2020-08-25 19:59:28 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
+window.onload = function() {
 
+(function () {
+   var time = '';
+   var sec = 0;
+   var min = 0;
+   setInterval(function() {
+       if (sec === 59) {
+          sec = 0;
+          min++;
+       } else {
+          sec++;
+       };
+       if (min<10) time = '0';
+       time += min + ':';
+       if (sec<10) time += '0';
+       time += sec;
+       timer.innerText = time;
+   },1000);
+})();
+
+var words = ['один','два','три'];
+
+if (!localStorage.words) {
+    localStorage.words = JSON.stringify(words);
+};
+
+words = JSON.parse(localStorage.words);
+
+word.innerText = words[~~(Math.random()*words.length)];
+cell.onclick = function() {
+  word.innerText = words[~~(Math.random()*words.length)];
+};
+
+};
