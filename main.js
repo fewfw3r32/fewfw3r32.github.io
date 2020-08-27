6a6ca1b5709a24def92cc9de7064378142cb1dda
fewfw3r32 <69639077+fewfw3r32@users.noreply.github.com>
2020-08-27 22:06:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,43 @@
 window.onload = function() {
 
-(function () {
-   var time = '';
-   var sec = 0;
-   var min = 0;
-   setInterval(function() {
-       if (sec === 59) {
-          sec = 0;
-          min++;
-       } else {
-          sec++;
-       };
-       if (min<10) time = '0';
-       time += min + ':';
-       if (sec<10) time += '0';
-       time += sec;
-       timer.innerText = time;
-   },1000);
-})();
-
-var words = ['один','два','три'];
-
-if (!localStorage.words) {
-    localStorage.words = JSON.stringify(words);
-};
-
-words = JSON.parse(localStorage.words);
-
-word.innerText = words[~~(Math.random()*words.length)];
-cell.onclick = function() {
-  word.innerText = words[~~(Math.random()*words.length)];
-};
-
+    (function() {
+        var time = '';
+        var sec = 0;
+        var min = 0;
+        setInterval(function() {
+            time = '';
+            if (sec === 59) {
+                sec = 0;
+                min++;
+            } else {
+                sec++;
+            };
+            if (min < 10) time = '0';
+            time += min + ':';
+            if (sec < 10) time += '0';
+            time += sec;
+            timer.innerText = time;
+        }, 1000);
+    })();
+    var words = ['один', 'два', 'три'];
+    var pos = ~~(Math.random() * words.length);
+    if (!localStorage.words) {
+        localStorage.words = JSON.stringify(words);
+    };
+    words = JSON.parse(localStorage.words);
+    word.innerText = words[pos];
+    roll.innerText = ['↑', '↓', '→'][~~(Math.random() * 3)];
+    cell.onclick = function(e) {
+        if (e.target.className == "remove") {
+            if (confirm('Удалить слово?')) {
+                words.splice(pos, 1);
+                localStorage.words = JSON.stringify(words);
+            };
+        } else if (e.target.className == "roll") {
+            roll.innerText = ['↑', '↓', '→'][~~(Math.random() * 3)];
+        } else {
+            pos = ~~(Math.random() * words.length);
+            word.innerText = words[pos];
+        };
+    };
 };
